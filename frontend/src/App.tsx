915826042ecc8a9cea4e19977c4eb8f34519b079
HP657 <junlee5070@gmail.com>
2024-07-08 16:49:22 +0900

    전체 app css및 틀

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Content from './Content/Content';
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Content />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
