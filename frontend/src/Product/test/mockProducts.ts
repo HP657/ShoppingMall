3250510c3dc39b800e2ef7ac4065d146d767781e
HP657 <junlee5070@gmail.com>
2024-07-10 19:26:19 +0900

    test데이터

---
+++ frontend/src/Product/test/mockProducts.ts
@@ -0,0 +1,124 @@
+const mockProducts = [
+    { 
+      id: 1, 
+      name: "Product 1", 
+      image: "https://via.placeholder.com/150", 
+      price: 10000, 
+      description: "상품 설명 1" 
+    },
+    { 
+      id: 2, 
+      name: "Product 2", 
+      image: "https://via.placeholder.com/150", 
+      price: 20000, 
+      description: "상품 설명 2" 
+    },
+    { 
+      id: 3, 
+      name: "Product 3", 
+      image: "https://via.placeholder.com/150", 
+      price: 30000, 
+      description: "상품 설명 3" 
+    },
+    { 
+      id: 4, 
+      name: "Product 4", 
+      image: "https://via.placeholder.com/150", 
+      price: 40000, 
+      description: "상품 설명 4" 
+    },
+    { 
+      id: 5, 
+      name: "Product 5", 
+      image: "https://via.placeholder.com/150", 
+      price: 50000, 
+      description: "상품 설명 5" 
+    },
+    { 
+      id: 6, 
+      name: "Product 6", 
+      image: "https://via.placeholder.com/150", 
+      price: 60000, 
+      description: "상품 설명 6" 
+    },
+    { 
+      id: 7, 
+      name: "Product 7", 
+      image: "https://via.placeholder.com/150", 
+      price: 70000, 
+      description: "상품 설명 7" 
+    },
+    { 
+      id: 8, 
+      name: "Product 8", 
+      image: "https://via.placeholder.com/150", 
+      price: 80000, 
+      description: "상품 설명 8" 
+    },
+    { 
+      id: 9, 
+      name: "Product 9", 
+      image: "https://via.placeholder.com/150", 
+      price: 90000, 
+      description: "상품 설명 9" 
+    },
+    { 
+      id: 10, 
+      name: "Product 10", 
+      image: "https://via.placeholder.com/150", 
+      price: 100000, 
+      description: "상품 설명 10" 
+    },
+    { 
+      id: 11, 
+      name: "Product 11", 
+      image: "https://via.placeholder.com/150", 
+      price: 110000, 
+      description: "상품 설명 11" 
+    },
+    { 
+      id: 12, 
+      name: "Product 12", 
+      image: "https://via.placeholder.com/150", 
+      price: 120000, 
+      description: "상품 설명 12" 
+    },
+    { 
+      id: 13, 
+      name: "Product 13", 
+      image: "https://via.placeholder.com/150", 
+      price: 130000, 
+      description: "상품 설명 13" 
+    },
+    { 
+      id: 14, 
+      name: "Product 14", 
+      image: "https://via.placeholder.com/150", 
+      price: 140000, 
+      description: "상품 설명 14" 
+    },
+    { 
+      id: 15, 
+      name: "Product 15", 
+      image: "https://via.placeholder.com/150", 
+      price: 150000, 
+      description: "상품 설명 15" 
+    },
+    { 
+      id: 16, 
+      name: "Product 16", 
+      image: "https://via.placeholder.com/150", 
+      price: 160000, 
+      description: "상품 설명 16" 
+    },
+    { 
+      id: 17, 
+      name: "Product 17", 
+      image: "https://via.placeholder.com/150", 
+      price: 170000, 
+      description: "상품 설명 17" 
+    },
+  ];
+  
+  export default mockProducts;
+  
