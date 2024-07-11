7b93bdaf11d6a4684af4e2b575528dbbd08642a8
HP657 <junlee5070@gmail.com>
2024-07-11 14:01:26 +0900

    로그인 회원가입 버튼 생성

--- frontend/src/Header/Header.tsx
+++ frontend/src/Header/Header.tsx
@@ -1,8 +1,25 @@
+import { useEffect, useState } from 'react';
 import styles from './Header.module.css';
 
 const Header = () => {
+  const [logging, setLogging] = useState(false);
+
+  useEffect(() => {
+    // Perform any effect or cleanup here
+  }, []);
+
   return (
-    <header className={styles.header}>헤더</header>
+    <header className={styles.header}>
+      <div className={styles.header_logo}>
+        로고
+      </div>
+      {!logging && (
+        <div className={styles.button_container}>
+          <button>로그인</button>
+          <button>회원가입</button>
+        </div>
+      )}
+    </header>
   );
 };
 
