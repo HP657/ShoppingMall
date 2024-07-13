6f3bfa136ac46c0c91d3bba79520f229412a7a58
HP657 <junlee5070@gmail.com>
2024-07-13 21:52:11 +0900

    로그인 및 회원가입 버튼에 Link to를 사용해 리다이렉트 기능 구현

--- frontend/src/Header/Header.tsx
+++ frontend/src/Header/Header.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
+import { useEffect, useState } from 'react';
 
 const Header = () => {
   const [logging, setLogging] = useState(false);
@@ -11,14 +12,20 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.header_logo}>
-        로고
+        <Link to="/">
+          <img src="/logo192.png" alt="Logo" />
+        </Link>
       </div>
-      {!logging && (
+      {logging ? '로그인 중' : 
         <div className={styles.button_container}>
-          <button>로그인</button>
-          <button>회원가입</button>
+          <Link to="/login" className={styles.link_button}>
+            로그인
+          </Link>
+          <Link to="/signup" className={styles.link_button}>
+            회원가입
+          </Link>
         </div>
-      )}
+      }
     </header>
   );
 };
