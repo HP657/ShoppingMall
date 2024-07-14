bccaef490d5b8d578a44cb36f6bc99ba1e80285e
HP657 <junlee5070@gmail.com>
2024-07-14 11:29:58 +0900

    로그인 폼

--- frontend/src/LogInForm/LogInForm.tsx
+++ frontend/src/LogInForm/LogInForm.tsx
@@ -1,9 +1,35 @@
-import styles from "./LogInForm.module.css"
+import styles from "./LogInForm.module.css";
+import { useState } from "react";
 
 function LogInForm() {
+    const [loginEmail, setLoginEmail] = useState<string>("");
+    const [loginPw, setLoginPw] = useState<string>("");
+
+    const handleLogin = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log({ loginEmail, loginPw });
+    };
+
     return (
-        <h1>로그인 페이지</h1>
-    )
+        <>
+            <h1>로그인</h1>
+            <form onSubmit={handleLogin}>
+                <input 
+                    type="text" 
+                    name="LoginEmail" 
+                    value={loginEmail} 
+                    onChange={(e) => setLoginEmail(e.target.value)} 
+                /><br />
+                <input 
+                    type="password" 
+                    name="LoginPw" 
+                    value={loginPw} 
+                    onChange={(e) => setLoginPw(e.target.value)} 
+                /><br />
+                <input type="submit" value="로그인하기" />
+            </form>
+        </>
+    );
 }
 
-export default LogInForm
+export default LogInForm;
