62f506d86e4b04ac6afc3655487f487a6bc56198
HP657 <junlee5070@gmail.com>
2024-07-14 11:30:03 +0900

    회원가입 폼

--- frontend/src/SignUpForm/SignupForm.tsx
+++ frontend/src/SignUpForm/SignupForm.tsx
@@ -1,9 +1,53 @@
-import styles from "./SignUpForm.module.css"
+import styles from "./SignUpForm.module.css";
+import { useState } from "react";
 
 function SignUpForm() {
+    const [userName, setUserName] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [passwordConfirm, setPasswordConfirm] = useState<string>("");
+
+    const handleSignUp = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log({ userName, email, password, passwordConfirm });
+    };
+
     return (
-        <h1>회원가입 페이지</h1>
-    )
+        <>
+            <h1>회원가입</h1>
+            <form onSubmit={handleSignUp}>
+                <input 
+                    type="text" 
+                    name="UserName" 
+                    placeholder="이름" 
+                    value={userName} 
+                    onChange={(e) => setUserName(e.target.value)} 
+                /><br />
+                <input 
+                    type="email" 
+                    name="Email" 
+                    placeholder="이메일" 
+                    value={email} 
+                    onChange={(e) => setEmail(e.target.value)} 
+                /><br />
+                <input 
+                    type="password" 
+                    name="Password" 
+                    placeholder="비밀번호" 
+                    value={password} 
+                    onChange={(e) => setPassword(e.target.value)} 
+                /><br />
+                <input 
+                    type="password" 
+                    name="PasswordConfirm" 
+                    placeholder="비밀번호 확인" 
+                    value={passwordConfirm} 
+                    onChange={(e) => setPasswordConfirm(e.target.value)} 
+                /><br />
+                <input type="submit" value="회원가입" />
+            </form>
+        </>
+    );
 }
 
-export default SignUpForm
+export default SignUpForm;
